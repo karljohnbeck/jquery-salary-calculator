45579919dac916f7653c2f10b735476b682d8295
Karl Beck <karljohnbeck@gmail.com>
2020-07-24 16:20:48 -0500

    made the submit button function but not append yet

--- client.js
+++ client.js
@@ -9,4 +9,16 @@ function readyNow() {
 
 function populateTable() {
     console.log('hurray')
+    let employeeData = {
+        firstName: $('#firstNameIn').val(),
+        lastName: $('#lastNameIn').val(),
+        idNumber: $('#idNumberIn').val(),
+        jobTitle: $('#jobTitleIn').val(),
+        annualSalary: $('#annualSalaryIn').val(),
+    }
+    employeeList.push(employeeData);
+    console.log(employeeList)
+    
 };
+
+let = employeeList = [];
