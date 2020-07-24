645c0c6739d1ecf58169a57570fd0fe30af11975
Karl Beck <karljohnbeck@gmail.com>
2020-07-24 16:37:39 -0500

    will append on the dom and clear input field

--- client.js
+++ client.js
@@ -18,7 +18,27 @@ function populateTable() {
     }
     employeeList.push(employeeData);
     console.log(employeeList)
-    
+
+    $('#employeeTableRows').empty();
+    for (let employee of employeeList) {
+        $('#employeeTableRows').append(`
+        <tr>
+            <td>${employee.firstName}</td>
+            <td>${employee.lastName}</td>
+            <td>${employee.idNumber}</td>
+            <td>${employee.jobTitle}</td>
+            <td>${employee.annualSalary}</td>
+            <td><button class="deleteEmployeeBtn">Delete</button></td>
+        </tr>`)
+    }
+
+    $('#firstNameIn').val('');
+    $('#lastNameIn').val('');
+    $('#idNumberIn').val('');
+        $('#jobTitleIn').val('');
+    $('#annualSalaryIn').val('');
+
 };
 
 let = employeeList = [];
+
