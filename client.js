db70b6cc3c033b595ef0657ef29f60706d36b9a4
Karl Beck <karljohnbeck@gmail.com>
2020-07-25 16:54:04 -0500

    compleated actual delete button functionality

--- client.js
+++ client.js
@@ -7,36 +7,41 @@ function readyNow() {
     $('#SubmitEmployeeData').on('click', populateTable)
     $('#employeeTableRows').on('click', '.deleteEmployeeBtn', removeEmployee)
 }
+
 let monthlyCosts = 0;
 function populateTable() {
     console.log('hurray')
+    
     let employeeData = {
         firstName: $('#firstNameIn').val(),
         lastName: $('#lastNameIn').val(),
         idNumber: $('#idNumberIn').val(),
         jobTitle: $('#jobTitleIn').val(),
         annualSalary: $('#annualSalaryIn').val(),
+        secretIndex: employeeList.length,
+
     }
     employeeList.push(employeeData);
     console.log(employeeList)
 
     $('#employeeTableRows').empty();
-    for (let employee of employeeList) {
+
+    for (let i = 0; i < employeeList.length; i++) {
         $('#employeeTableRows').append(`
         <tr>
-            <td>${employee.firstName}</td>
-            <td>${employee.lastName}</td>
-            <td>${employee.idNumber}</td>
-            <td>${employee.jobTitle}</td>
-            <td>${employee.annualSalary}</td>
-            <td><button class="deleteEmployeeBtn">Delete</button></td>
+            <td>${employeeList[i].firstName}</td>
+            <td>${employeeList[i].lastName}</td>
+            <td class="employeeID">${employeeList[i].idNumber}</td>
+            <td>${employeeList[i].jobTitle}</td>
+            <td>${employeeList[i].annualSalary}</td>
+            <td><button class="deleteEmployeeBtn btn btn-danger">Delete</button></td>
         </tr>`)
     }
     let monthly = 0;
     for (let employee of employeeList) {
         monthly += Number(employee.annualSalary)
     }
-    let monthlyCosts = monthly/12
+    let monthlyCosts = (monthly / 12)
 
     if (monthlyCosts > 20000) {
         $('#monthlyCosts').addClass("redBackground");
@@ -50,12 +55,33 @@ function populateTable() {
     // $('#idNumberIn').val('');
     // $('#jobTitleIn').val('');
     // $('#annualSalaryIn').val('');
- console.log(monthlyCosts)
+    console.log(monthlyCosts)
 };
 
 let = employeeList = [];
 
 function removeEmployee() {
-    console.log('BYE!')
+    
+    console.log($(this).closest('tr').index())
+    
+    let index = ($(this).closest('tr').index())
+
+    employeeList.splice(index, 1)
+    
     $(this).closest('tr').remove()
+    console.log(employeeList)
+
+    let monthly = 0;
+    for (let employee of employeeList) {
+        monthly += Number(employee.annualSalary)
+    }
+    let monthlyCosts = (monthly / 12)
+
+    if (monthlyCosts > 20000) {
+        $('#monthlyCosts').addClass("redBackground");
+    }
+
+    $('#monthlyCosts').empty()
+    $('#monthlyCosts').append(`<h3 id="monthlyCosts">Total Monthly Cost: $${monthlyCosts}</h3>`)
+
 }
