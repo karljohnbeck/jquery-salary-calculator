78332bbddc46f172daf6eefb430902c0abe68433
Karl Beck <karljohnbeck@gmail.com>
2020-07-25 21:03:38 -0500

    cleaned up the code a bit but still need to add notes

--- README.md
+++ README.md
@@ -1,9 +1,36 @@
-# Project Name
-
-[Project Instructions](./INSTRUCTIONS.md), this line may be removed once you have updated the README.md
+# Project Name: Salary Calculator
 
 ## Description
 
-Your project description goes here. What problem did you solve? How did you solve it?
+_Duration: 1 weekend_
 
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+This project was to creat a web page where you would be able to input employee information and store the data on the webpage. To accomplish this we created input fields for employee first name, last name, employee ID number, job title, and yearly salary that would all be stored in an array. From the collection of data the webpage would calculate the monthly cost of all employees and will display a red total monthly cost if it ever exceeds $20,000. where is also a feture to delete employee information from the page with a 'delete' button next tot the employees information on the table. 
+
+## Screen Shot
+
+-Screen Shot-
+
+## Usage
+How does someone use this application? Tell a user story here.
+
+1. go to file:///Users/karlbeck/Prime-Tier-2/Week-7/jquery-salary-calculator/index.html
+2. Input employee information into the fields 
+3. Hit the 'Submit' button when all fields are filled out 
+4. If you need to remove an employee from the list, hit the 'delete' button on the employees infromation row on the table 
+
+
+
+## Built With
+
+- JavaScript
+- Bootstrap
+- JQuery
+- Html
+- CSS
+
+
+## Acknowledgement
+Thanks to [Prime Digital Academy](www.primeacademy.io) who equipped and helped me to make this application a reality. (Thank your people)
+
+## Support
+If you have suggestions or issues, please email me at [karljohnbeck@gmail.com]
--- client.js
+++ client.js
@@ -11,63 +11,70 @@ function readyNow() {
 let monthlyCosts = 0;
 function populateTable() {
     console.log('hurray')
-    
-    let employeeData = {
-        firstName: $('#firstNameIn').val(),
-        lastName: $('#lastNameIn').val(),
-        idNumber: $('#idNumberIn').val(),
-        jobTitle: $('#jobTitleIn').val(),
-        annualSalary: $('#annualSalaryIn').val(),
-        secretIndex: employeeList.length,
+    if ($('#firstNameIn').val() === '' ||
+        $('#lastNameIn').val() === '' ||
+        $('#idNumberIn').val() === '' ||
+        $('#jobTitleIn').val() === '' || 
+        $('#annualSalaryIn').val() === '') {
+        alert('Please fill out all inputs')
+    } else {
+        let employeeData = {
+            firstName: $('#firstNameIn').val(),
+            lastName: $('#lastNameIn').val(),
+            idNumber: $('#idNumberIn').val(),
+            jobTitle: $('#jobTitleIn').val(),
+            annualSalary: $('#annualSalaryIn').val(),
+            secretIndex: employeeList.length,
 
-    }
-    employeeList.push(employeeData);
-    console.log(employeeList)
+        }
+        employeeList.push(employeeData);
+        console.log(employeeList)
 
-    $('#employeeTableRows').empty();
+        $('#employeeTableRows').empty();
 
-    for (let i = 0; i < employeeList.length; i++) {
-        $('#employeeTableRows').append(`
+        for (let i = 0; i < employeeList.length; i++) {
+            $('#employeeTableRows').append(`
         <tr>
             <td>${employeeList[i].firstName}</td>
             <td>${employeeList[i].lastName}</td>
             <td class="employeeID">${employeeList[i].idNumber}</td>
             <td>${employeeList[i].jobTitle}</td>
-            <td>${employeeList[i].annualSalary}</td>
+            <td>$${employeeList[i].annualSalary}</td>
             <td><button class="deleteEmployeeBtn btn btn-danger">Delete</button></td>
         </tr>`)
-    }
-    let monthly = 0;
-    for (let employee of employeeList) {
-        monthly += Number(employee.annualSalary)
-    }
-    let monthlyCosts = (monthly / 12)
+        }
+        let monthly = 0;
+        for (let employee of employeeList) {
+            monthly += Number(employee.annualSalary)
+        }
+        let monthlyCosts = (monthly / 12)
 
-    if (monthlyCosts > 20000) {
-        $('#monthlyCosts').addClass("redBackground");
+        if (monthlyCosts > 20000) {
+            $('#monthlyCosts').addClass("redBackground");
+        }
+
+        $('#monthlyCosts').empty()
+        $('#monthlyCosts').append(`<h3 id="monthlyCosts">Total Monthly Cost: $${monthlyCosts}</h3>`)
+
+        $('#firstNameIn').val('');
+        $('#lastNameIn').val('');
+        $('#idNumberIn').val('');
+        $('#jobTitleIn').val('');
+        $('#annualSalaryIn').val('');
+        console.log(monthlyCosts)
     }
-
-    $('#monthlyCosts').empty()
-    $('#monthlyCosts').append(`<h3 id="monthlyCosts">Total Monthly Cost: $${monthlyCosts}</h3>`)
-
-    // $('#firstNameIn').val('');
-    // $('#lastNameIn').val('');
-    // $('#idNumberIn').val('');
-    // $('#jobTitleIn').val('');
-    // $('#annualSalaryIn').val('');
-    console.log(monthlyCosts)
 };
 
 let = employeeList = [];
 
 function removeEmployee() {
-    
+
     console.log($(this).closest('tr').index())
-    
+
     let index = ($(this).closest('tr').index())
 
     employeeList.splice(index, 1)
-    
+
     $(this).closest('tr').remove()
     console.log(employeeList)
 
@@ -79,6 +86,8 @@ function removeEmployee() {
 
     if (monthlyCosts > 20000) {
         $('#monthlyCosts').addClass("redBackground");
+    }else {
+        $('#monthlyCosts').removeClass("redBackground");
     }
 
     $('#monthlyCosts').empty()
